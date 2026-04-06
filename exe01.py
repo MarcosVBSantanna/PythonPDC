9eb585e38395585017e8cffcd788d49d7a009fec
MarcosVBSantanna <marcosvbsantanna@gmail.com>
2026-04-06 20:17:15 -0300

    Initial commit: exercise files

---
+++ exe01.py
@@ -0,0 +1,26 @@
+"""
+Se é maior que zero
+
+numero = 10
+if(numero > 0):
+    print("Valor informado é maior que zero!")
+"""
+"""
+verificar se o numero é par ou impar
+
+numero = int(input("Entre com um numero:"))
+if(numero % 2 == 0):
+    print(numero, " é par!")
+else:
+    print(numero, " é impar!")
+"""
+
+a = int(input("Entre com o valor de a:"))
+b = int(input("Entre com o valor de b:"))
+
+if(a > b):
+    print("Valor de a é maior que b!")
+elif(a < b):
+    print("Valor de b é maior que a!")
+else:
+    print("Os valores de a e b são iguais!")
